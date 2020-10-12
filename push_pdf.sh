61ee9a4c2010e6fd3b71b4cc9702d73c092f9ec9
Axel Mendoza <axel.mendoza@epita.fr>
2020-10-12 15:59:26 -0500

    fix push_pdf.sh

--- push_pdf.sh
+++ push_pdf.sh
@@ -8,9 +8,13 @@ printf "\033[0;32mCompile and push pdf to GitHub...\033[0m\n"
 pdflatex main.tex
 
 # Add changes to git.
-git add main.text
+git add main.pdf
 
 # Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
 git commit -m "$msg"
 
 # Push source and build repos.
